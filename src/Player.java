bca747358e1efbd1bc9a13012c0b5735900b46c7
FilipeGomesMelo <61971614+FilipeGomesMelo@users.noreply.github.com>
2021-11-12 14:40:48 -0300

    Update Player.java

--- src/Player.java
+++ src/Player.java
@@ -1,3 +1,4 @@
+import ui.AddSongWindow;
 import ui.PlayerWindow;
 
 import java.time.Duration;
@@ -6,6 +7,10 @@ import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Player {
 
@@ -14,29 +19,28 @@ public class Player {
     private boolean isPlaying = false;
     private boolean isRepeat = false;
     private double currentTime = 0;
-    private int currentId = -1;
+    private String currentId = "-1";
+    private int idCounter = 0;
     private Instant start;
     private Instant stop;
 
+    private AddSongWindow addSongWindow = null;
 
-    String [][] Queue = {
-            {"Mr. Blue Sky", "Out Of The Blue", "Electric Light Orchestra", "1977", "05:02", "302", "0"},
-            {"Carry On Wayward Son", "Leftoverture", "Kansas", "1976", "05:25", "325", "1"}
-    };
+    private final Lock lock = new ReentrantLock();
+
+    private Map<String, String[]> Musicas = new LinkedHashMap<String, String[]>();
+
+    String [][] Queue = {};
 
     public Player() {
         ActionListener buttonListenerPlayNow = e -> start();
 
-        ActionListener buttonListenerRemove = e -> {
-            System.out.println("buttonListenerRemove");
-        };
-        ActionListener buttonListenerAddSong = e -> {
-            System.out.println("buttonListenerAddSong");
-        };
-        ActionListener buttonListenerPlayPause = e -> {
-            this.isPlaying = !this.isPlaying;
-            this.playerWindow.updatePlayPauseButton(this.isPlaying);
-        };
+        ActionListener buttonListenerRemove = e -> removeSong();
+
+        ActionListener buttonListenerAddSong = e -> addSong();
+
+        ActionListener buttonListenerPlayPause = e -> playPause();
+
         ActionListener buttonListenerStop = e -> {
             System.out.println("buttonListenerStop");
         };
@@ -52,6 +56,7 @@ public class Player {
         ActionListener buttonListenerRepeat = e -> {
             System.out.println("buttonListenerRepeat");
         };
+
         MouseListener scrubberListenerClick = new MouseListener() {
             @Override
             public void mouseClicked(MouseEvent e) {
@@ -96,57 +101,115 @@ public class Player {
                 scrubberListenerClick,
                 scrubberListenerMotion,
                 "Tittle",
-                Queue
+                this.Queue
         );
 
         playerWindow.start();
-
+        this.start = Instant.now();
         while (true) {
-            System.out.println(this.isPlaying);
-            if (this.isPlaying) {
-                if (this.currentTime > Integer.parseInt(Queue[this.currentId][5])) {
-                    this.isPlaying = false;
-                }
+            this.stop = Instant.now();
+            Duration dt = Duration.between(this.start, this.stop);
+            if (dt.getSeconds() >= 1) {
+                this.start = Instant.now();
+                this.lock.lock();
+                if (this.isPlaying) {
+                    if (this.currentTime >= Integer.parseInt(this.Musicas.get(this.currentId)[5])) {
+                        this.isPlaying = false;
+                        this.currentTime -= 1;
+                    }
+                    this.currentTime += 1;
+                    this.stop = Instant.now();
 
-                this.stop = Instant.now();
-                Duration dt = Duration.between(this.start, this.stop);
-
-                this.playerWindow.updateMiniplayer(
-                        this.isActive,
-                        this.isPlaying,
-                        this.isRepeat,
-                        (int) this.currentTime,
-                        Integer.parseInt(this.Queue[this.currentId][5]),
-                        this.currentId,
-                        this.Queue.length
-                );
-                if (dt.getSeconds() > 1) {
-                    this.start = Instant.now();
-                    this.currentTime += 2;
+                    this.playerWindow.updateMiniplayer(
+                            this.isActive,
+                            this.isPlaying,
+                            this.isRepeat,
+                            (int) this.currentTime,
+                            Integer.parseInt(this.Musicas.get(this.currentId)[5]),
+                            Integer.parseInt(this.currentId),
+                            this.Queue.length
+                    );
                 }
+                this.lock.unlock();
             }
         }
     }
 
     public void start() {
-        this.currentId = this.playerWindow.getSelectedSongID();
+        this.lock.lock();
+        this.currentId = String.valueOf(this.playerWindow.getSelectedSongID());
         this.currentTime = 0;
-        this.playerWindow.updatePlayingSongInfo(this.Queue[currentId][0],
-                this.Queue[currentId][1],
-                this.Queue[currentId][2]);
+        this.playerWindow.updatePlayingSongInfo(this.Musicas.get(this.currentId)[0],
+                this.Musicas.get(this.currentId)[1],
+                this.Musicas.get(this.currentId)[2]);
         this.playerWindow.updateMiniplayer(
                 this.isActive,
                 this.isPlaying,
                 this.isRepeat,
                 (int) this.currentTime,
-                Integer.parseInt(this.Queue[currentId][5]),
-                this.currentId,
+                Integer.parseInt(this.Musicas.get(this.currentId)[5]),
+                Integer.parseInt(this.currentId),
                 this.Queue.length
                 );
         this.playerWindow.enableScrubberArea();
         this.start = Instant.now();
         this.isPlaying = true;
         this.playerWindow.updatePlayPauseButton(this.isPlaying);
+        this.lock.unlock();
+    }
+
+    public void playPause() {
+        this.lock.lock();
+        this.isPlaying = !this.isPlaying;
+        this.start = Instant.now();
+        this.playerWindow.updatePlayPauseButton(this.isPlaying);
+        this.lock.unlock();
+    }
+
+    public void addSong() {
+        ActionListener buttonListenerAddSongOK = a -> {
+            this.lock.lock();
+            String [] song = this.addSongWindow.getSong();
+            this.Musicas.put(String.valueOf(this.idCounter), song);
+            this.idCounter += 1;
+            String [][] newQueue = new String[this.Musicas.size()][7];
+            int count = 0;
+            for (Map.Entry<String, String[]> entry: this.Musicas.entrySet()) {
+                newQueue[count] = entry.getValue();
+                count += 1;
+            }
+            this.Queue = newQueue;
+            playerWindow.updateQueueList(this.Queue);
+            this.addSongWindow.interrupt();
+            this.addSongWindow = null;
+            this.lock.unlock();
+        };
+        this.lock.lock();
+        this.addSongWindow = new AddSongWindow(String.valueOf(this.idCounter),
+                buttonListenerAddSongOK,
+                this.playerWindow.getAddSongWindowListener());
+
+        addSongWindow.start();
+        this.lock.unlock();
+    }
+
+    public void removeSong() {
+        this.lock.lock();
+        if (String.valueOf(this.playerWindow.getSelectedSongID()).equals(this.currentId)){
+            this.playerWindow.resetMiniPlayer();
+            this.currentTime = 0;
+            this.isPlaying = false;
+        }
+        this.Musicas.remove(String.valueOf(this.playerWindow.getSelectedSongID()));
+        this.idCounter += 1;
+        String [][] newQueue = new String[this.Musicas.size()][7];
+        int count = 0;
+        for (Map.Entry<String, String[]> entry: this.Musicas.entrySet()) {
+            newQueue[count] = entry.getValue();
+            count += 1;
+        }
+        this.Queue = newQueue;
+        playerWindow.updateQueueList(this.Queue);
+        this.lock.unlock();
     }
 }
-
